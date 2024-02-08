dcb7e45c37881cd29b42066a4fd989bdfd33d2d9
Ildrassil <schneiderjakob7@googlemail.com>
2024-02-08 13:46:22 +0100

    RezepteController
    MongoDb Uri implemented
    Rezept Controller
    UnitTests
    Todos left for Backend:
    -[] Integration Test
    
    Signed-off-by: Ildrassil <schneiderjakob7@googlemail.com>

--- backend/src/test/java/org/schmeckis/backend/UnitTest/RezeptServiceTest.java
+++ backend/src/test/java/org/schmeckis/backend/UnitTest/RezeptServiceTest.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class RezeptServiceTest {
+class RezeptServiceTest {
 
     private final RezeptRepo rezeptRepo = mock(RezeptRepo.class);
     private final IdService idService = mock(IdService.class);

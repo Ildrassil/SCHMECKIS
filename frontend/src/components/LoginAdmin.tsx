1b68c3183c9fc63e6525ee78dda4d93e86e789d7
Ildrassil <144778160+Ildrassil@users.noreply.github.com>
2024-02-22 13:40:44 +0100

    Admin LoginAdmin.tsx
    Security Config erstellt
    Customized Endpunkt
    Sowie Password Encoder
    Context Menu für AnchorPunkt des Admin Logins
    
    Fehlt styling des Login Interfaces
    
    Fehlt immer noch RichtextEditor converter
    
    Fehlt ChatGptApi transcriptor von Links
    
    Signed-off-by: Ildrassil <144778160+Ildrassil@users.noreply.github.com>

--- frontend/src/components/LoginAdmin.tsx
+++ frontend/src/components/LoginAdmin.tsx
@@ -48,34 +48,38 @@ export default function Login({setLoggedIn}: LoginProps) {
     }
 
     return (
-        <div className="w-full h-full self-center flex">
-            <div className="flex-col justify-center self-center align-middle w-1/3 h-fit shadow-doubleOut">
+        <div className="flex flex-wrap w-full h-full self-center flex justify-center bg-transparent">
+            <div
+                className=" flex flex-wrap flex-col p-10 justify-items-center rounded-2xl content-center justify-center items-center  self-center align-middle w-1/3 h-fit shadow-doubleOut">
                 {displayError && <p className=" justify-center text-center text-red-600
             text-4xl ">Invalid username or password</p>}
                 <h1 className="text-textHeader text-4xl text-center">Login</h1>
-                <form className="flex-col" onSubmit={() => {
+                <form className="flex-col align-middle self-center justify-center" onSubmit={() => {
                 }}>
-                    <label className=" flex-row text-center text-lg font-semibold text-textHeader"
-                           htmlFor="username">Username:</label>
-                    <input
-                        className=" flex-row text-center border-none active:border-none hover:border-none border-transparent text-lg font-semibold text-textHeader bg-offWhite hover:shadow-hashtagbutton active:shadow-hashtagbutton"
-                        type="text" id="username" name="username"
-                        onChange={onUsernameChange} value={password} required/>
-
-                    <label className=" flex-row text-center text-lg font-semibold text-textHeader"
-                           htmlFor="password">Password:</label>
-                    <input
-                        className="flex-row text-center border-none active:border-none hover:border-none border-transparent text-lg font-semibold text-textHeader bg-offWhite hover:shadow-hashtagbutton active:shadow-hashtagbutton"
-                        type="password" id="password" name="password"
-                        value={password} onChange={onPasswordChange} required/>
-                    <div className="flex-row justify-center">
-                        <button className="content-center hover:shadow-hashtagbutton active:shadow-hashtagbutton
-                shadow-hashtagbuttonOut bg-offWhite w-fit text-center text-xl
-                flex-col" type="button" onClick={authenticate}>Login
+                    <div className="flex-row justify-center mx-5 mt-5">
+                        <label className="text-center text-lg font-semibold text-textHeader"
+                               htmlFor="username">Username:</label>
+                        <input
+                            className=" flex-row text-center border-none active:border-none hover:border-none border-transparent text-lg font-semibold text-textHeader bg-offWhite hover:shadow-hashtagbutton active:shadow-hashtagbutton"
+                            type="text" id="username" name="username"
+                            onChange={onUsernameChange} value={password} required/>
+                    </div>
+                    <div className="flex-row justify-center mx-5 mt-5">
+                        <label className=" flex-row text-center text-lg font-semibold text-textHeader"
+                               htmlFor="password">Password:</label>
+                        <input
+                            className="flex-row text-center  border-none active:border-none hover:border-none border-transparent text-lg font-semibold text-textHeader bg-offWhite hover:shadow-hashtagbutton active:shadow-hashtagbutton"
+                            type="password" id="password" name="password"
+                            value={password} onChange={onPasswordChange} required/>
+                    </div>
+                    <div className="flex-row justify-items-center content-center">
+                        <button className="justify-items-center items-center content-center  hover:shadow-hashtagbutton active:shadow-hashtagbutton
+                shadow-hashtagbuttonOut bg-offWhite w-fit text-center mx-4 px-4  my-10 rounded text-xl
+                " type="button" onClick={cancel}>Cancel
                         </button>
-                        <button className="content-center hover:shadow-hashtagbutton active:shadow-hashtagbutton
-                shadow-hashtagbuttonOut bg-offWhite w-fit text-center text-xl
-                flex-col" type="button" onClick={cancel}>Cancel
+                        <button className="items-center justify-items-center content-center px-4 mx-4 rounded hover:shadow-hashtagbutton active:shadow-hashtagbutton
+                shadow-hashtagbuttonOut bg-offWhite w-fit text-center text-xl my-10
+                " type="button" onClick={authenticate}>Login
                         </button>
                     </div>
                 </form>

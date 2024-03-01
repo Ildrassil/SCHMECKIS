6376eddfc7aab2d89ea1816f8a8308155dfc5cd8
Ildrassil <144778160+Ildrassil@users.noreply.github.com>
2024-03-01 13:36:27 +0100

    Exception Handler ErrorMessage DTO
    Signed-off-by: Ildrassil <144778160+Ildrassil@users.noreply.github.com>

---
+++ backend/src/main/java/org/schmeckis/backend/controller/ExceptionController.java
@@ -0,0 +1,27 @@
+package org.schmeckis.backend.controller;
+
+import org.schmeckis.backend.model.dto.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorMessage globalIllegalArgumentExceptionHandler(IllegalArgumentException exception) {
+        return new ErrorMessage(exception.getMessage());
+    }
+
+    @ExceptionHandler({NoSuchElementException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage globalNoSuchElementExceptionHandler(NoSuchElementException exception) {
+        return new ErrorMessage(exception.getMessage());
+    }
+
+
+}
